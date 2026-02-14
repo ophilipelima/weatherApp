a1946650c57a28afd65224c7ce717a708df35b28
ophilipelima <philipehenri.lima@gmail.com>
2026-02-14 18:08:14 -0300

    componente curretWeather completo

--- src/app/components/current-weather/current-weather.component.ts
+++ src/app/components/current-weather/current-weather.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-current-weather',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './current-weather.component.html',
   styleUrl: './current-weather.component.scss'
 })
 export class CurrentWeatherComponent {
   @Input() city!: string;
   @Input() temperature?: number;
+  dtAtual = new Date();
 }
--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -14,29 +14,30 @@ export class HomeComponent implements OnInit {
 
   constructor(private weatherService: WeatherapiService){}
 
-  city = 'Berlim, Alemanha';
+  city = 'Berlin, Germany';
   temperature?: number;
   wind = 0;
   humidity = 46;
   precipitation = 0; 
   isDay = 0;
-
+  
 
   ngOnInit(): void {
-    this.carregarTempo(52.5186, 13.4081);
+    this.carregarTempo(-15.7797, -47.9297);
   }
 
   carregarTempo(lat: number, long: number){
     this.weatherService.getWeather(lat, long).subscribe(
       {
         next: (resposta) => {
-        this.temperature = resposta.current?.temperature_2m;
+        this.temperature = Math.round(resposta[1].current?.temperature_2m);
         this.humidity = resposta.current?.relative_humidity_2m;
         this.wind = resposta.current?.wind_speed_10m;
         this.precipitation = resposta.current?.precipitation;
         this.isDay = resposta.current?.is_day;
-          console.log('RESPOSTA COMPLETA:', resposta);
-          console.log('TEMPERATURA:', resposta.current?.temperature_2m);
+        this.city = resposta[1].timezone;
+        console.log('Resposta', resposta)
+        console.log('TEMPERATURA:', resposta.current?.temperature_2m);
         },
 
         error: (err) => {
