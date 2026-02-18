b47fd14671e42db1c4024a540f49336d6c3ef87f
ophilipelima <philipehenri.lima@gmail.com>
2026-02-17 23:49:48 -0300

    inserindo campo de pesquisa

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from '../../components/search/search.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
